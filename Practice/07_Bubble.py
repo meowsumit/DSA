a9a20f1f7fba39981c534a45b5109e8a6a0f0b01
Sumit Kumar <kumarsk2sumit@gmail.com>
2026-02-28 17:29:51 +0530

    updating

---
+++ Practice/07_Bubble.py
@@ -0,0 +1,13 @@
+def Bubb(nums):
+    n = len(nums)
+    for i in range(n - 2, -1, -1):
+        is_swap = False
+        for j in range(i + 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+                is_swap = True
+        if is_swap == False:
+            break
+    return nums
+nums = [1,2,3,4,5,6,7]
+print (Bubb(nums))
